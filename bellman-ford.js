90ab3f5cad2115049b4be02c9463c712ec96cd9b
Evgeniy Kuznetsov <chamscontacts@gmail.com>
2015-04-26 16:49:53 +0300

    Implementation without cycle check

--- bellman-ford.js
+++ bellman-ford.js
@@ -7,10 +7,88 @@
 
 // Bellman Ford algorithm
 // @param {Edges} edges
+// @param {Vertex} source
 // @param {Vertex} destination
-// @return {Integer|false} Integer - length of a path, false - graph has negative cycle
-function bellmanFord(edges, destination) {
-  return 0;
+// @return {Integer|undefined} Integer - length of a path, undefined - graph has negative cycle
+function bellmanFord(edges, source, destination) {
+
+  var vertices = calculateVertices(edges);
+  var verticesNames = Object.keys(vertices);
+
+  // 2D array for collections solutions to subproblems
+  var solutions = [];
+
+  var createSolutionEntryIfNot = function(i) {
+    if(!solutions[i]) solutions[i] = [];
+  }
+
+  // Initialize solutions array
+  createSolutionEntryIfNot(0);
+  for(var i = 0; i < verticesNames.length; i++) {
+    solutions[0][verticesNames[i]] = Infinity;
+  }
+  solutions[0][source] = 0;
+
+  // Main loop
+  // i = 1 because we already calculated for i = 0
+  for(var i = 1; i < edges.length; i++) {
+
+    // Throw away unneeded solutions
+    delete solutions[i - 2];
+
+    createSolutionEntryIfNot(i);
+
+    for(var j = 0; j < verticesNames.length; j++) {
+      var cv = verticesNames[j];
+
+      var currentSolutions = [];
+
+      for(var z = 0; z < vertices[cv].headTo.length; z++) {
+        var currentHead = vertices[cv].headTo[z];
+        var val = solutions[i - 1][currentHead[0]] + currentHead[1];
+
+        currentSolutions.push(val);
+      }
+
+      solutions[i][cv] = Math.min(
+        solutions[i - 1][cv],
+        Math.min.apply(this, currentSolutions)
+      );
+
+    }
+
+  }
+
+  var nocycle = true;
+
+  return nocycle ? solutions[solutions.length - 2][destination] : undefined;
+}
+
+
+// section: Helpers
+
+// Calculate vertices information
+// @param {Array} edges
+// @return {Object}
+function calculateVertices(edges) {
+  var r = {};
+
+  var createEntryIfNot = function(vertex) {
+    if(!r[vertex]) r[vertex] = {headTo: [], tailTo: []};
+  }
+
+  for(var i = 0; i < edges.length; i++) {
+    var currentEdge = edges[i];
+
+    createEntryIfNot(currentEdge[0]);
+    createEntryIfNot(currentEdge[1]);
+
+    r[currentEdge[0]].tailTo.push([currentEdge[1], currentEdge[2]]);
+    r[currentEdge[1]].headTo.push([currentEdge[0], currentEdge[2]]);
+
+  }
+
+  return r;
 }
 
 
@@ -22,7 +100,18 @@ var edges = [
   [2, 3, 1],
   [2, 4, 2],
   [3, 5, 4],
-  [5, 6, 2]
+  [4, 5, 2]
 ];
 
-console.log("Case 1:", bellmanFord(edges, 5) == 6, bellmanFord(edges, 5));
+var edgesWithNegCycle = [
+  [1, 2, 2],
+  [1, 3, 4],
+  [2, 3, 1],
+  [2, 4, 2],
+  [3, 5, 4],
+  [4, 5, 2],
+  [5, 1, -7]
+];
+
+console.log("Case 1:", bellmanFord(edges, 1, 5) == 6, bellmanFord(edges, 1, 5));
+console.log("Case 2:", bellmanFord(edgesWithNegCycle, 1, 5) == undefined, bellmanFord(edges, 1, 5));
